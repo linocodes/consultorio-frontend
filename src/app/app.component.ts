4b7a54073d04e63bc3c898457f757316a560a4fb
Marcelo Lino <marcelo.lino.santos@gmail.com>
2018-01-28 23:11:54 -0200

    Interceptar Htpp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'app';
+
+  constructor(private httpClient: HttpClient) { }
+
+  method1Call(): void {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/users').subscribe(
+      success => {
+        console.log('Successfully Completed');
+        console.log(success);
+      }
+    );
+  }
+
+  method2Call(): void {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/user12').subscribe(
+      success => {
+        console.log('Successfully Completed');
+        console.log(success);
+      }
+    );
+  }
+
+  method3Call(): void {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/comments?postId=1').subscribe(
+      success => {
+        console.log('Successfully Completed');
+        console.log(success);
+      }
+    );
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
+import { AppHttpInterceptor } from './app-http-interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AppHttpInterceptor,
+      multi: true
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
